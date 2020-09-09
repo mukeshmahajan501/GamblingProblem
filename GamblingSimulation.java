5cca33adcf4268f5024a0039d8f7c618c89aafcd
mukesh <mukeshmahajan501@gmail.com>
2020-09-09 16:33:31 +0530

    [mukesh:modified]-gambler win or lost for month

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,35 +1,41 @@
-
 public class GamblingSimulation {
 	// declare constant variable
 	final int STAKE = 100;
 	final int BET = 1;
 	final int WINING_CASH = 150;
 	final int LOOSING_CASH = 50;
-	final int NO_OF_DAYS = 20;
-
-	int result;
 
 	public void checkWinLoose() {
 
-		for (int i = 1; i <= NO_OF_DAYS; i++) {
-			result = STAKE;
+		for (int i = 1; i <= 30; i++) {
+			int winCount = 0;
+			int lostCount = 0;
+			int result = STAKE;
+
+			int totalAmount = 0;
 			while ((result != WINING_CASH) && (result != LOOSING_CASH)) {
 				int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
 
 				if (randomCheck == 1) {
 					result = result + BET;
+					winCount++;
 
 				} else {
 					result = result - BET;
+					lostCount++;
 				}
 
 			}
 
 			if (result == WINING_CASH) {
-				System.out.println("total Amount won in day:" + i + " amount: " + result);
+				System.out.println("you won the game");
+				totalAmount = winCount;
+				System.out.println("total Amount won in day:" + i + " Amount:" + totalAmount);
 
 			} else {
-				System.out.println("total amount lost in days:" + i + " amount: " + result);
+				System.out.println("you loose the game");
+				totalAmount = lostCount;
+				System.out.println("total amount lost in days: " + i + " Amount:" + totalAmount);
 			}
 
 		}
